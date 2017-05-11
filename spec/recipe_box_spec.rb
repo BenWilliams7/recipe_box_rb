44082d09e16cd83789a824834c886fc1b2010ebb
Ben Williams and Kat Gifford <kat@>
2017-05-11 10:41:53 -0700

    add passing spec for ingredient

--- spec/recipe_box_spec.rb
+++ spec/recipe_box_spec.rb
@@ -1,14 +1,19 @@
 require "spec_helper"
 
-describe(Category) do
+describe Category do
   it { should have_and_belong_to_many(:recipes) }
 end
 
 describe Instruction do
-  it {should belong_to :recipe}
+  it { should belong_to :recipe}
 end
 
-# describe Recipe do
-#   it { should have_and_belong_to_many :categories}
-#   it { should have_and_belong_to_many :ingredients}
-# end
+describe Recipe do
+  it { should have_and_belong_to_many :categories}
+  it { should have_and_belong_to_many :ingredients}
+  it { should have_many :instructions}
+end
+
+describe Ingredient do
+  it { should have_and_belong_to_many :recipes}
+end
