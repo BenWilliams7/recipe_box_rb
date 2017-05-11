0d56271820f595c5f3c4dc7accf58345a4609e79
Ben Williams and Kat Gifford <kat@>
2017-05-11 11:23:10 -0700

    add update/patch to ingredients

--- app.rb
+++ app.rb
@@ -31,10 +31,14 @@ post "/ingredients" do
   recipe_id = params.fetch("recipe_id")
   recipe = Recipe.find(recipe_id)
   ingredient_name = params.fetch("ingredient-name")
-  # recipe.ingredients.create(name: ingredient_name)
-
   new_ingredient = Ingredient.create(name: ingredient_name)
   redirect "/recipe/edit/#{recipe_id}"
+end
+
+get "/ingredient/:id" do
+  ingredient_id = params.fetch("id")
+  @ingredient = Ingredient.find(ingredient_id)
+  erb(:ingredient_edit)
 end
 
 get "/recipe/edit/:id" do
@@ -49,4 +53,11 @@ post "/instructions" do
   instruction_desc = params.fetch("instruction-desc")
   Instruction.create(name: instruction_desc, recipe_id: recipe_id)
   redirect "/recipe/edit/#{recipe_id}"
+end
+
+patch "/ingredient/:id/update" do
+  ingredient_name = params.fetch("ingredient-name")
+  @ingredient = Ingredient.find(params.fetch("id").to_i)
+  @ingredient.update({name: ingredient_name})
+  redirect "/recipes"
 end
